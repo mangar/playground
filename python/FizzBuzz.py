29301b1f8dcea682375c9df4b7c714d6c184d7b7
mmangar <marcio.mangar@gmail.com>
2020-12-27 20:50:58 -0300

    assert

--- python/FizzBuzz.py
+++ python/FizzBuzz.py
@@ -9,6 +9,8 @@ config = _config['DEFAULT']
 flag_debug = False
 
 
+#
+#
 def index(fizz=config['Fizz'], buzz=config['Buzz'], fizzbuzz=config['FizzBuzz']):
 
     out = []
@@ -39,6 +41,15 @@ def index(fizz=config['Fizz'], buzz=config['Buzz'], fizzbuzz=config['FizzBuzz'])
 
 
 
+#
+#
+def index_test():
+    result = index(fizz="xxx")['out']
+    assert result[0] == 1
+    assert result[2] == "xxx"
+
+
+
 
 
 
@@ -59,5 +70,10 @@ for o in result["out"]:
     print(">> " + str(o))
 
 print("-------")
+
+
+print("Test:")
+index_test()
+
 
 
